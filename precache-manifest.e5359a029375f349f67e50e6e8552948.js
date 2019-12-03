bbc972d7b5a417eeacadb21094cbc5895a2309d3
Kyle <wilichowskikyle@gmail.com>
2019-12-03 16:29:27 -0600

    Updates

---
+++ precache-manifest.e5359a029375f349f67e50e6e8552948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123f1010dd97360267282314f8c15162",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c78dfd07b30d96de479",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b0fb120175ca89f844b7",
+    "url": "/static/js/2.b898528c.chunk.js"
+  },
+  {
+    "revision": "3c78dfd07b30d96de479",
+    "url": "/static/js/main.7e8106fe.chunk.js"
+  },
+  {
+    "revision": "ae970b72d1121cf53cb6",
+    "url": "/static/js/runtime-main.3664b334.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
